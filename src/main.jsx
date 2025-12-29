0fb330e17d353358b8850898781a1eb17da35a7f
Sok Kimsour <sok.kimsour12042003@gmail.com>
2025-12-29 14:15:39 +0700

    Fix deploy : add file for deploy gitlab

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/Apple-Clone">
+        <BrowserRouter basename="/apple-clone">
             <App />
         </BrowserRouter>
     </React.StrictMode>
